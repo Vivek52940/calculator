a0f776d030ff7126cf00f64e19a941988b227634
vivek singh <imvsingh64@gmail.com>
2020-07-21 17:45:10 +0530

    final

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ console.log("Calculator");
 function insert(num){
     document.form.textview.value =  document.form.textview.value+num;
 }
+function equal(){
+    var exp = document.form.textview.value;
+    if(exp){
+        document.form.textview.value=eval(exp);
+    }
+    
+}
+function clr(){
+    document.form.textview.value = "";
+}
+function back(){
+    var exp= document.form.textview.value;
+    document.form.textview.value= exp.substring(0,exp.length-1);
+}
